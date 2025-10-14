0686151bf7294687580d806fb4c627864a947727
cags9607 <44386058+cags9607@users.noreply.github.com>
2025-10-14 14:44:40 -0600

    Update __init__.py

--- meta_tag_classifier/__init__.py
+++ meta_tag_classifier/__init__.py
@@ -1,3 +1,21 @@
-__all__ = ["__version__", "clean_dataframe", "embed_texts", "train", "load_predictor", "Predictor"]
+# src/meta_tag_classifier/__init__.py  (or meta_tag_classifier/__init__.py if not using src/)
+__all__ = [
+    "__version__",
+    "clean_dataframe",
+    "embed_texts",
+    "train",
+    "load_predictor",
+    "Predictor",
+    "svm_predictor",
+]
+
 __version__ = "0.1.1"
-from .api import clean_dataframe, embed_texts, train, load_predictor, Predictor
+
+from .api import (
+    clean_dataframe,
+    embed_texts,
+    train,
+    load_predictor,
+    Predictor,
+    svm_predictor,
+)
