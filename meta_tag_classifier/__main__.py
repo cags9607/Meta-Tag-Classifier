b5d2dbfaf8971aacdee56337b372cc7b9d530b5d
Sergio Carrasco <44386058+cags9607@users.noreply.github.com>
2026-06-08 20:06:33 -0600

    removing files to upload new repo

--- meta_tag_classifier/__main__.py
+++
@@ -1,4 +0,0 @@
-from meta_tag_classifier.cli import main
-
-if __name__ == "__main__":
-    main()
--- scripts/clean_data.py
+++
@@ -1,5 +0,0 @@
-from meta_tag_classifier.cli import main
-if __name__ == "__main__":
-    import sys
-    sys.argv = ["mtc", "clean", "--input", "data/raw/train.csv", "--output", "data/processed/train_clean.csv"]
-    main()
